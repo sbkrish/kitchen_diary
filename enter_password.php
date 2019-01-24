7c5ec9cb040831e67c8aa1328234f570d9c082d3
Balakrishnan Subramaniyan <sbkrish@users.noreply.github.com>
2019-01-24 11:20:03 +0530

    Update enter_password.php
    Password field is modified with PIN restricted with 4 digits.
    Submit button is removed and auto submit is newly added with the use of JS.

--- enter_password.php
+++ enter_password.php
@@ -4,7 +4,7 @@ $name=$_SESSION['name'];
 $user=implode($name);
 if(!($user))
 {
-	header('location:home.php');
+	echo '<script> window.location.href="home.php?st"</script>';
 }
 ?>
 
@@ -25,9 +25,17 @@ if(!($user))
   height: 100%;	
   
 }
+h4{
+	margin-top: 150px;
+	color: grey;
+}
+img[src="https://cdn.000webhost.com/000webhost/logo/footer-powered-by-000webhost-white2.png"]
+{
+  display: none;
+}
 
 .panel{
-	margin-top: 180px;
+	margin-top: 30px;
 	font-size: 16px;
 }
 
@@ -40,10 +48,32 @@ button{
 	display: block;
 	margin-top: 10px;
 }
-input{
+
+input[type="number"]{
 	display: block;
 	text-align: center;
 	margin-left: 15%;
+	margin-top: 5%;
+	letter-spacing: 20px ;
+	font-size: 24px;
+
+}
+
+input[type="Number"]::-webkit-inner-spin-button,
+input[type="Number"]::-webkit-outer-spin-button {
+	-webkit-appearance: none;
+	-moz-appearance: none;
+	appearance: none;
+	margin: 0;
+}
+
+input[type="password"]{
+	display: block;
+	text-align: center;
+	margin-left: 15%;
+	margin-top: 5%;
+	letter-spacing: 20px ;
+	font-size: 24px;
 
 }
 .input-sm{
@@ -59,10 +89,37 @@ input{
 
 a
 {
-	font-size: 12px;
+	font-size: 14px;
 }
 
 </style>
+<script>
+	/*function changeToPassword() {
+    setTimeout(function () {
+        document.getElementById("password").setAttribute("type", "password")
+    }, 50);
+var passLength = document.getElementById("password").value.length;
+if (passLength == 4) {
+	document.getElementById("myform").submit();
+}
+}
+function changeToNumber() {
+	setTimeout(function () {
+        document.getElementById("password").setAttribute("type", "number")
+    }, 1);
+    
+}*/
+
+function formSubmit() {
+	var passLength = document.getElementById("password").value.length;
+	
+	if (passLength == 4) {
+	document.getElementById("myform").submit();
+}
+}
+
+
+</script>
 </head>
 <body>
 	
@@ -70,15 +127,17 @@ a
 	<div class="row">
 		<div class="col-sm-6 col-sm-offset-3">
   <div class="row">
+  	<h4>Enter PIN</h4>
     <div class="panel">
 
   <div class="panel-body">
 
-<form method="post" action="password_exist.php">
+<form method="post" action="password_exist.php" id="myform">
 	
 	<p>Hi <?php echo $user; ?>,</p>
-	<input type="password" class="form-control input-sm" name="password" placeholder="Give me your Password" required="" autofocus="">
-	<button class="btn btn-primary btn-sm">Sign in</button><br><hr>
+	<input type="password" class="form-control input-sm" id="password" name="password" placeholder="" required="" autofocus="" onkeyup="formSubmit();">
+	<!-- <button class="btn btn-primary btn-sm">Sign in</button><br><hr> -->
+	<hr>
 	<a  href="home.php" style="text-decoration: none;"><< Back</a>
 	<a class="col-sm-6" href="logout.php" style="text-decoration: none;">Refresh</a>
 </form>	
